0ce64dc3c1d085137ee4bb1d37c6893896582f9e
Rahul Kandel <rahulkandel7@gmail.com>
2022-12-15 12:16:22 +0545

    make sidebar fixed

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -90,79 +90,81 @@ function Sidebar() {
 
   return (
     <>
-      <div
-        className={`${sidebar ? "w-24" : "w-64"
-          } flex-shrink-0 shadow-md min-h-screen max-h-fit bg-gray-50 relative`}
-      >
-        <button
-          className={`absolute top-[3%]  p-2 rounded-full bg-white shadow-md hover:shadow cursor-pointer ${sidebar ? "-right-[20%]" : "-right-[7%]"
-            }`}
-          onClick={() => {
-            if (sessionStorage.getItem("sidebar")) {
-              sessionStorage.clear();
-            } else {
-              sessionStorage.setItem("sidebar", true);
-            }
-            setSidebar(!sidebar);
-          }}
+      <div className="w-64 flex-shrink-0">
+        <div
+          className={`${sidebar ? "w-24" : "w-64"
+            } flex-shrink-0 shadow-md max-h-screen overflow-scroll bg-gray-50 fixed`}
         >
-          <RiMenu2Fill className=" text-gray-800 text-lg" />
-        </button>
-        {sidebar ? (
-          <div className="mt-5 mb-3 flex items-center justify-center">
-            <div className="w-12 rounded-full h-full">
-              <img
-                src="/logo.png"
-                alt="Logo"
-                className="w-12 h-12 object-contain  mx-auto rounded-full border border-gray-100 shadow-lg "
-              />
-            </div>
-          </div>
-        ) : (
-          <div className="mt-5 mb-3 flex items-center justify-center">
-            <div className="w-36 rounded-full h-full">
-              <img
-                src="/logo.png"
-                alt="Logo"
-                className="w-36 h-36 object-contain  mx-auto rounded-full border border-gray-100 shadow-lg "
-              />
-            </div>
-          </div>
-        )}
-
-        {sidebar ? null : (
-          <div>
-            <h1 className="text-lg font-bold text-gray-600 text-center">
-              Welcome To
-            </h1>
-            <h1 className="text-sm font-semibold text-gray-400 text-center">
-              Admin Panel
-            </h1>
-          </div>
-        )}
-        <hr className="mt-3" />
-        <div className="mt-2 text-lg">
-          {menuItem.map((item, index) => (
-            <NavLink
-              key={index}
-              className={({ isActive }) =>
-                isActive
-                  ? "border-l-4 bg-indigo-500 rounded-md mx-4 py-3 text-white  border-l-indigo-800 shadow-md px-3   flex items-center"
-                  : " flex px-3 py-3 my-1 mx-4 items-center hover:bg-indigo-100 hover:rounded-md hover:shadow border-l-4 border-l-transparent hover:border-l-indigo-500"
-              }
-              to={item.link}
-            >
-              {item.icon} {sidebar ? null : item.name}
-            </NavLink>
-          ))}
-
           <button
-            className="flex px-3 py-3 my-1 mx-4 items-center hover:bg-indigo-100 hover:rounded-md hover:shadow border-l-4 border-l-transparent hover:border-l-indigo-500 w-10/12"
-            onClick={logout}
+            className={`absolute top-[3%]  p-2 rounded-full bg-white shadow-md hover:shadow cursor-pointer ${sidebar ? "-right-[20%]" : "-right-[0%]"
+              }`}
+            onClick={() => {
+              if (sessionStorage.getItem("sidebar")) {
+                sessionStorage.clear();
+              } else {
+                sessionStorage.setItem("sidebar", true);
+              }
+              setSidebar(!sidebar);
+            }}
           >
-            <IoLogOutOutline className="mr-2" />{" "}
-            {sidebar ? null : <span>Logout</span>}
+            <RiMenu2Fill className=" text-gray-800 text-lg" />
           </button>
+          {sidebar ? (
+            <div className="mt-5 mb-3 flex items-center justify-center">
+              <div className="w-12 rounded-full h-full">
+                <img
+                  src="/logo.png"
+                  alt="Logo"
+                  className="w-12 h-12 object-contain  mx-auto rounded-full border border-gray-100 shadow-lg "
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="mt-5 mb-3 flex items-center justify-center">
+              <div className="w-36 rounded-full h-full">
+                <img
+                  src="/logo.png"
+                  alt="Logo"
+                  className="w-36 h-36 object-contain  mx-auto rounded-full border border-gray-100 shadow-lg "
+                />
+              </div>
+            </div>
+          )}
+
+          {sidebar ? null : (
+            <div>
+              <h1 className="text-lg font-bold text-gray-600 text-center">
+                Welcome To
+              </h1>
+              <h1 className="text-sm font-semibold text-gray-400 text-center">
+                Admin Panel
+              </h1>
+            </div>
+          )}
+          <hr className="mt-3" />
+          <div className="mt-2 text-lg ">
+            {menuItem.map((item, index) => (
+              <NavLink
+                key={index}
+                className={({ isActive }) =>
+                  isActive
+                    ? "border-l-4 bg-indigo-500 rounded-md mx-4 py-3 text-white  border-l-indigo-800 shadow-md px-3   flex items-center"
+                    : " flex px-3 py-3 my-1 mx-4 items-center hover:bg-indigo-100 hover:rounded-md hover:shadow border-l-4 border-l-transparent hover:border-l-indigo-500"
+                }
+                to={item.link}
+              >
+                {item.icon} {sidebar ? null : item.name}
+              </NavLink>
+            ))}
+
+            <button
+              className="flex px-3 py-3 my-1 mx-4 items-center hover:bg-indigo-100 hover:rounded-md hover:shadow border-l-4 border-l-transparent hover:border-l-indigo-500 w-10/12"
+              onClick={logout}
+            >
+              <IoLogOutOutline className="mr-2" />{" "}
+              {sidebar ? null : <span>Logout</span>}
+            </button>
+          </div>
         </div>
       </div>
     </>
